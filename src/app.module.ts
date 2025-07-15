30dd0451c3255bcc9a405aa4a1dd8ac8e45239cb
showemeowkx <sashacherepov55@gmail.com>
2025-07-15 21:37:20 +0200

    Add ApolloDriver to the App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      driver: ApolloDriver,
       autoSchemaFile: true,
     }),
     LessonModule,
