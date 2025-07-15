a87f8d497bf6b97306746f96542a382e0b98f0a3
showemeowkx <sashacherepov55@gmail.com>
2025-07-15 22:14:17 +0200

    Implement createLesson method

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
   @Query((returns) => LessonType)
   lesson() {
     return {
@@ -12,5 +14,14 @@ export class LessonResolver {
       startDate: new Date().toISOString(),
       endDate: new Date().toDateString(),
     };
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
