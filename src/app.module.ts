c2378be07741148f46812a5464fd5bf04551dbb4
showemeowkx <sashacherepov55@gmail.com>
2025-07-15 21:48:43 +0200

    Add TypeOrmModule to the App module and create Lesson entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
+import { Lesson } from './lesson/lesson.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: '../.env' }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: `mongodb://${process.env.DB_HOST}/school`,
+      synchronize: true,
+      entities: [Lesson],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       driver: ApolloDriver,
